efb273704eda429762ef7b4025fe18a21131d4f2
swade781977 <swade781977@yahoo.com>
2021-02-06 20:32:56 -0500

    commit tree fb497625f6c237539585240c43ee57a3edf2f348

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    let arr = []
+    sourceArray.forEach(element => {
+        arr.push(element * -1)
+    })
+    return arr
+}
+
+function mapToNoChange(sourceArray){
+    let arr = []
+    sourceArray.forEach(element => {
+        arr.push(element)
+    })
+    return arr
+}
+
+function mapToDouble(sourceArray){
+    let arr = []
+    sourceArray.forEach(element => {
+        arr.push(element * 2)
+    })
+    return arr
+}
+
+function mapToSquare(sourceArray){
+    let arr = []
+    sourceArray.forEach(element => {
+        arr.push(element * element)
+    })
+    return arr
+}
+
+function reduceToTotal(sourceArray, startingPoint= 0){
+    let total = startingPoint
+    for(let i = 0; i < sourceArray.length; i++){
+        total = sourceArray[i] + total;
+    }
+    return total
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
